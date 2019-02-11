07bff03fa93da18881283597f3c604448ada7ea9
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-11 23:30:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,19 +14,19 @@ client.on('message', message => {
 
 
 client.on('message', async message => {
-    if(message.content.startsWith(prefix + "bcall")) {
-      let i = client.users.size;
-      if(message.author.id !== '411255565499629579') return message.channel.send(':negative_squared_cross_mark: » هذا الأمر مخصص لصاحب البوت فقط');
-      var args = message.content.split(' ').slice(1).join(' ');
-      if(!args) return message.channel.send(':negative_squared_cross_mark: » يجب عليك كتابة الرسالة')
-      setTimeout(() => {
-        message.channel.send(تم الارسال لـ ${i} شخص)
-      }, client.users.size * 500);
-      client.users.forEach(s => {
-        s.send(args).catch(e => i--);
-      });
-    }
-    });
+if(message.content.startsWith(prefix + "bcall")) {
+  let i = client.users.size;
+  if(message.author.id !== '321476121956974595') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+  var args = message.content.split(' ').slice(1).join(' ');
+  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+  setTimeout(() => {
+    message.channel.send(`تم الارسال لـ ${i} شخص`)
+  }, client.users.size * 500);
+  client.users.forEach(s => {
+    s.send(args).catch(e => i--);
+  });
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);
