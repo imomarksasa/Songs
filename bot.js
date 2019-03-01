d78e07268f9812a4f5440c61274d69befd105f49
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-03-01 23:59:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,20 +63,7 @@ if (message.content.startsWith('setavatar')) {
 }
 });
 
-client.on('message', async message => {
-if(message.content.startsWith(prefix + "omg")) {
-  let i = client.users.size;
-  if(message.author.id !== '321476121956974595') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
-  var args = message.content.split(' ').slice(1).join(' ');
-  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
-  setTimeout(() => {
-    message.channel.send(`تم الارسال لـ ${i} شخص`)
-  }, client.users.size * 500);
-  client.users.forEach(s => {
-    s.send(args).catch(e => i--);
-  });
-}
-});
+
 
 
 client.login(process.env.BOT_TOKEN);
